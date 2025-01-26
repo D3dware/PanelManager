cae2eaf3838cdb3840b0961f743f748def15f788
Kelvin Santos <146021430+D3dware@users.noreply.github.com>
2025-01-25 21:21:53 -0300

    Add files via upload

---
+++ src/models/painel.java
@@ -0,0 +1,393 @@
+package models;
+
+/**
+ *
+ * @author D3dware
+ */
+public class painel {
+    
+    //atributos
+    private String id;
+    private String lote;
+    private String marca;
+    private String amostra;
+    private String ABO;
+    private String RH;
+    private String sequencia;
+
+    //RH
+    private String D;
+    private String C;
+    private String c_pequeno;
+    private String E;
+    private String e_pequeno;
+    private String f;
+    private String CW;
+    
+    //KELL
+    private String kpa;
+    private String kpb;
+    private String K;
+    private String Cellano;
+    private String JSA;
+    private String JSB;
+    
+    //DIEGO
+    private String dia;
+    
+    //LUTHERAN
+    private String lua;
+    private String lub;
+    
+    //COLT
+    private String Cob;
+    
+    //XG
+    private String Xga;
+    
+    //DUFFY
+    private String fya;
+    private String fyb;
+    
+    //KIDD
+    private String jka;
+    private String jkb;
+    
+    //MNS
+    private String M;
+    private String N;
+    private String S;
+    private String s;
+    
+    //PP1PK
+    private String p1;
+    private String IR_P1;
+    
+    //LEWIS
+    private String lea;
+    private String leb;
+    
+    //genotipagem
+    private String genotipagem;
+   
+    //OUTROS
+    private String observacoes;
+    //
+    //
+
+    
+    //--------------------------------------------------------------------//
+    //getters e setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAmostra() {
+        return amostra;
+    }
+
+    public void setAmostra(String amostra) {
+        this.amostra = amostra;
+    }
+    
+    public String getABO() {
+        return ABO;
+    }
+
+    public void setABO(String ABO) {
+        this.ABO = ABO;
+    }
+    
+    public String getRH() {
+        return RH;
+    }
+
+    public void setRH(String RH) {
+        this.RH = RH;
+    }
+    
+    public String getSequencia() {
+        return sequencia;
+    }
+
+    public void setSequencia(String sequencia) {
+        this.sequencia = sequencia;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String D) {
+        this.D = D;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String C) {
+        this.C = C;
+    }
+
+    public String getC_pequeno() {
+        return c_pequeno;
+    }
+
+    public void setC_pequeno(String c_pequeno) {
+        this.c_pequeno = c_pequeno;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String E) {
+        this.E = E;
+    }
+
+    public String getE_pequeno() {
+        return e_pequeno;
+    }
+
+    public void setE_pequeno(String e_pequeno) {
+        this.e_pequeno = e_pequeno;
+    }
+    
+    public String getKpa() {
+        return kpa;
+    }
+
+    public void setKpa(String kpa) {
+        this.kpa = kpa;
+    }
+
+    public String getKpb() {
+        return kpb;
+    }
+
+    public void setKpb(String kpb) {
+        this.kpb = kpb;
+    }
+
+    public String getK() {
+        return K;
+    }
+
+    public void setK(String K) {
+        this.K = K;
+    }
+
+    public String getCellano() {
+        return Cellano;
+    }
+
+    public void setCellano(String Cellano) {
+        this.Cellano = Cellano;
+    }
+    
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getLua() {
+        return lua;
+    }
+
+    public void setLua(String lua) {
+        this.lua = lua;
+    }
+
+    public String getLub() {
+        return lub;
+    }
+
+    public void setLub(String lub) {
+        this.lub = lub;
+    }
+
+    public String getFya() {
+        return fya;
+    }
+
+    public void setFya(String fya) {
+        this.fya = fya;
+    }
+
+    public String getFyb() {
+        return fyb;
+    }
+
+    public void setFyb(String fyb) {
+        this.fyb = fyb;
+    }
+
+    public String getJka() {
+        return jka;
+    }
+
+    public void setJka(String jka) {
+        this.jka = jka;
+    }
+
+    public String getJkb() {
+        return jkb;
+    }
+
+    public void setJkb(String jkb) {
+        this.jkb = jkb;
+    }
+
+    public String getM() {
+        return M;
+    }
+
+    public void setM(String M) {
+        this.M = M;
+    }
+
+    public String getN() {
+        return N;
+    }
+
+    public void setN(String N) {
+        this.N = N;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String S) {
+        this.S = S;
+    }
+
+    public String gets() {
+        return s;
+    }
+
+    public void sets(String s) {
+        this.s = s;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getLea() {
+        return lea;
+    }
+
+    public void setLea(String lea) {
+        this.lea = lea;
+    }
+
+    public String getLeb() {
+        return leb;
+    }
+
+    public void setLeb(String leb) {
+        this.leb = leb;
+    }
+
+    public String getGenotipagem() {
+        return genotipagem;
+    }
+
+    public void setGenotipagem(String genotipagem) {
+        this.genotipagem = genotipagem;
+    }
+    
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    
+    public String getF() {
+        return f;
+    }
+
+    public void setF(String f) {
+        this.f = f;
+    }
+
+    public String getCW() {
+        return CW;
+    }
+
+    public void setCW(String CW) {
+        this.CW = CW;
+    }
+
+    public String getJSA() {
+        return JSA;
+    }
+
+    public void setJSA(String JSA) {
+        this.JSA = JSA;
+    }
+
+    public String getJSB() {
+        return JSB;
+    }
+
+    public void setJSB(String JSB) {
+        this.JSB = JSB;
+    }
+
+    public String getCob() {
+        return Cob;
+    }
+
+    public void setCob(String Cob) {
+        this.Cob = Cob;
+    }
+
+    public String getXga() {
+        return Xga;
+    }
+
+    public void setXga(String Xga) {
+        this.Xga = Xga;
+    }
+
+    public String getIR_P1() {
+        return IR_P1;
+    }
+
+    public void setIR_P1(String IR_P1) {
+        this.IR_P1 = IR_P1;
+    }
+}
